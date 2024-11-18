05ed29ccb38bc3b70c36e9132fedb7ba0995cbb5
M.V <marco.mov98@gmail.com>
2024-11-18 08:50:38 +0000

    First Commit del ultra proyecto

--- src/app.js
+++ src/app.js
@@ -4,8 +4,20 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { FlatESLint } from "eslint/use-at-your-own-risk";
 
 window.onload = function() {
-  //write your code here
+  let pronounList = ["the", "our"];
+  let adjetiveList = ["great", "big"];
+  let nounsList = ["jogger", "racoon"];
+
+  pronounList.forEach(pronoun => {
+    adjetiveList.forEach(adjetive => {
+      nounsList.forEach(noun => {
+        console.log(`${pronoun}${adjetive}${noun}.com`);
+      });
+    });
+  });
+
   console.log("Hello Rigo from the console!");
 };
